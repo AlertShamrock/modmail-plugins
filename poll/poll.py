249d525e80e7291f7763574c0f2aac5c54368975
AlertShamrock <37253108+AlertShamrock@users.noreply.github.com>
2020-01-27 16:17:54 -0500

    Update poll.py

--- poll/poll.py
+++ poll/poll.py
@@ -80,8 +80,7 @@ class Polls(commands.Cog):
         embed = discord.Embed(color=self.bot.main_color, timestamp=datetime.datetime.utcnow(), description=f"**{question}**\n{answer}")
         embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
         poll = await ctx.send(embed=embed)
-       await ctx.send("<@&660677269769027584>")
-      await ctx.message.delete()
+       await ctx.send("<@&660677269769027584>").then(ctx.message.delete())
         for emoji, _ in answers:
             await poll.add_reaction(emoji)
 
@@ -135,8 +134,7 @@ class Polls(commands.Cog):
             embed = discord.Embed(color=self.bot.main_color, timestamp=datetime.datetime.utcnow(), description=f"**{question}**\n{body}")
             embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
             poll = await ctx.send(embed=embed)
-            await ctx.send("<@&660677269769027584>")
-            await ctx.message.delete()
+            await ctx.send("<@&660677269769027584>").then(ctx.message.delete())
             for emoji, _ in choices:
                 await poll.add_reaction(emoji)
 
